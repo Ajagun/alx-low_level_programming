88cc4854757a3df7fe258fa75dfb8ff6e29e8d3e
ajagun <jamiuajagunjeun@gmail.com>
2022-12-15 08:30:20 -0800

    pojrgt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
- * main - prints _putchar,
- * followed by a new line
- * Return: Always 0 (Success)
- */
+* main - prints _putchar,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-printf(" _putchar\n");
+printf("_putchar\n");
 return (0);
 }
