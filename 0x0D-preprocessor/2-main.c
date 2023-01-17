a8e8cfc0ea2a2606c0056fe6a7e2ec066027eae7
ajagun <jamiuajagunjeun@gmail.com>
2023-01-16 19:11:57 -0800

    okay

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the source file
- * Return: returns 0 always
- */
-int main()
+* main - prints the source file
+* Return: returns 0 always
+*/
+int main(void)
 {
 printf("%s", __FILE__);
 return (0);
